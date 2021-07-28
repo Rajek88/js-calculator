491235d9f51b2127560a22dd053485822be46acf
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-07-28 13:46:22 +0530

    Completed Final project with JavaScript

---
+++ js/calculator.js
@@ -0,0 +1,27 @@
+function clicked(btn){ 
+    var displayValue = document.querySelector('#input-field').value;
+    let putter = btn.getAttribute('data-value');
+    document.querySelector('#input-field').value = document.querySelector('#input-field').value + putter; 
+    console.log(putter);
+    console.log(displayValue);
+}
+
+function getAns(){
+    let finalAns = 0;
+    try {
+        finalAns = eval(document.querySelector('#input-field').value);
+        if(finalAns == undefined){
+            window.alert("Wrong Syntax, Please provide a valid input !")
+            return;
+        }
+
+    } catch (error) {
+        window.alert("Wrong Syntax, Please check your expression and try again ! \nError Details -> " + error);
+        return;
+    }
+    document.querySelector('#input-field').value = finalAns;
+}
+
+function resetAll(){
+    document.querySelector('#input-field').value = null;
+}
